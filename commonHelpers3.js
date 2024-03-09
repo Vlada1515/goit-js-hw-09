114dad8c46084175aa19111f234e51d34d960f9e
Vlada1515 <148682469+Vlada1515@users.noreply.github.com>
2024-03-09 13:30:08 +0000

    Deploying to gh-pages from @ Vlada1515/goit-js-hw-09@66feb216cf29f772e0197618c66df4b184a03659 🚀

---
+++ commonHelpers3.js
@@ -0,0 +1,2 @@
+import"./assets/styles-261518e3.js";const n=document.querySelectorAll(".linkList");n.forEach(e=>{e.addEventListener("click",t=>{t.preventDefault();const r=e.querySelector("a").getAttribute("href");window.location.href=r})});
+//# sourceMappingURL=commonHelpers3.js.map
